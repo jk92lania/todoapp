4acb53de8d10c95d8f9e7838e13474941f87d2de
jk92lania <jk92lania@gmail.com>
2022-04-05 16:47:02 +0900

    emit 및 props

--- src/App.vue
+++ src/App.vue
@@ -1,160 +1,62 @@
 <template>
-<div class="red">
- {{ 이름 }} {{ 나이 }}
-</div>
- <!-- {{ 안녕() }} -->
- <!-- {{ 이름말해봐('iU')}} -->
- <!-- {{ greeting }} -->
- {{ 정보.이름 }} {{정보.나이 }}
- <button v-on:click="log" class="btn btn-primary mt-2">logclick</button>
- <button v-on:click="updateInfo" class="btn btn-primary mt-2">click</button>
- <button v-on:click="updateAge" class="btn btn-primary mt-2 ml-2">click</button>
-  <input type="text" v-model="이름">
-
-  <div class="container">
+  <div class="container">    
     <h1>TODO LIST</h1>
-    <form action="" class="d-flex" v-on:submit.prevent>
-      <div class="flex-grow-1 mr-2">
-        <input 
-          type="text" 
-          placeholder="Type New Todo"
-          class="form-control"
-          v-model="todo"
-        >
-      </div>
-      <div>
-        <button class="btn btn-primary" type="submit" v-on:click="onSubmit">Add</button>
-      </div>
-    </form>
-  <div v-show="hasError" class="red">내용을 입력하세요</div>
-  <div 
-  class="card mt-2" 
-  v-for="(todo, index) in todos"
-  v-bind:key ="todo.id"
-  >
-    <div class="car-body p-2 d-flex">
-      <div class="form-check flex-grow-1">
-        <input 
-        type="checkbox" 
-        class="from-check-input" 
-        v-model="todo.complete"
-        v-bind:id="todo.id"
-        >
-        <!-- <label class="form-check-label" v-bind:style="todo.complete ? todoStyle : {}">{{ todo.subject }}</label> -->
-        <label 
-        class="form-check-label" 
-        v-bind:class="{todocss : todo.complete}"
-        v-bind:for="todo.id"
-        >
-        {{ todo.subject }}
-        </label>
-      </div>
-      <!-- 삭제버튼 -->
-      <div>
-        <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
-      </div>
-    </div>
-  </div>
-  </div>
-  <div v-show="toggle">true</div>
-  <div v-show="!toggle">false</div>
-  <button v-on:click="toggleFn">toggle</button>
+    <!-- 할일 추가 입력창 -->
+    <TodoSimpleForm v-on:add-todo="addTodo"/>
+    
 
+   
+    <!-- 목록없음 안내창 -->
+    <div v-if="!todos.length" class="red">생성된 Todo 목록이 없습니다.</div>
 
-  
+    <!-- todo 목룍창 -->
+    <TodoList v-bind:todos="todos"/>
+    
+  </div>  
 
 
 </template>
 
 <script>
-import {ref, reactive} from 'vue';
-export default {
-  setup(){
-    // const 이름 = '홍길동';
-    // const 나이 = 20;
-    // const 안녕 = function(){
-    //   return '반가워요';
-    // }
-    // const 이름말해봐 = function(_name){
-    //   return _name + '는 인사했습니다';
-    // }
-    // const greeting = 이름말해봐(이름);
-    
-    const 이름 = ref('홍길동');
-    const 나이 = ref(20);
-    const 정보 = reactive({
-      이름 : '춘향',
-      나이 : '13'
-    });
-    const log = () => {
-      console.log(이름.value);
-    }
-    const updateName = (event) => {
-      console.log(event.target.value);
-      이름.value = event.target.value;
-    }
-    const updateAge = () => {
-      나이.value = 30;
-    }
-    const updateInfo = () => {
-      정보.이름 = '향단이';
-    }
+import {ref} from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue'
+import TodoList from './components/TodoList.vue'
 
-    const todos = ref([
-      {id : 1, subject: '할일 1', complete:false },
-      {id : 2, subject: '할일 2', complete:false },
-      {id : 3, subject: '할일 3', complete:true },
-      {id : 4, subject: '할일 4', complete:false }
-    ]);
-    const todo = ref('');
+export default {
+  components : {
+    TodoSimpleForm,
+    TodoList
+  },
+
+  setup(){    
+
+    // 할일 목록(배열)을 저장
+      const todos = ref([
+        {id:1, subject:'할일', completed: false}
+      ]);
+
     const todoStyle = {
       textDecoration: 'line-through',
       color: 'gray'
     }
-    const onSubmit = () => {
-      if(todo.value === '') {
-        hasError.value = true;
-      } else {
-        // 안내창 안내
-        hasError.value = false;
-      console.log(todo.value);
-      todos.value.push({
-        id: Date.now(),
-        subject : todo.value,
-        complete : false
-      });
-      }
+
+    // TodoSimpleForm 에서  
+      // add-todo 이벤트로 전달된 객체를 
+      // 처리해 주는 콜백함수
+    const addTodo = (todo) => {
+      todos.value.push(todo);
     }
-    const hasError = ref(false);
     
-    const toggle = ref(false);
-    const toggleFn = () => {
-      toggle.value = !toggle.value;
-    }
     const deleteTodo = (index) => {
-      console.log('delete' + index);
+      // console.log('delete' + index);
       todos.value.splice(index, 1);
     }
 
 
     return {
-      이름,
-      나이,
-      // 안녕,
-      // 이름말해봐,
-      // greeting,
-      log,
-      updateName,
-      정보,
-      updateAge,
-      updateInfo,
       todos,
-      todo,
-      onSubmit,
-      toggle,
-      toggleFn,
-      hasError,
       todoStyle,
+      addTodo,
       deleteTodo
     }
   }
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,70 @@
+<template>
+    <!-- html 태그 작성 장소 -->
+    Todo 입력 폼
+    <form action="" v-on:submit.prevent>
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+            <input 
+            type="text" 
+            placeholder="Type New Todo"
+            class="form-control"
+            v-model="todo"
+            >
+        </div>
+        <div>
+            <button class="btn btn-primary" type="submit" v-on:click="onSubmit">Add</button>
+        </div>
+      </div>
+     <!-- 내용 입력 안내창 -->
+  <div v-show="hasError" class="red">내용을 입력하세요</div>
+    </form>
+
+
+
+</template>
+<script>
+import {ref} from 'vue'
+export default {
+    setup(props, context) {
+        
+        const todo = ref('');
+        // 내용 입력 안내창 여부
+        const hasError = ref(false);
+
+        // 버튼 클릭 시 처리 
+        const onSubmit = () => {
+            if(todo.value === '') {
+                hasError.value = true;
+            } else {
+                // 안내창 숨기기
+                hasError.value = false;
+                // 할일의 내용을 배열로 추가한다
+                // 이 때 상위 컴포넌트로 전달
+                // context.emit('이벤트', {데이터});
+                context.emit('add-todo', {
+                    id : Date.now(),
+                    subject : todo.value,
+                    complete : false
+                });
+
+                // console.log(todo.value);
+                // todos.value.push({
+                //     id: Date.now(),
+                //     subject : todo.value,
+                //     complete : false
+                // });
+            }
+            todo.value = '';
+        }
+
+        return {
+            todo,
+            hasError,
+            onSubmit
+        }
+    }
+}
+</script>
+<style>
+/* css 작성장소 */
+</style>
